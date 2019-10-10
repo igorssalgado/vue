5ed8221edd92469e003f9a1d10b21ec942311ccf
Igor Dos Santos Salgado <isantos@br.ibm.com>
2019-10-10 14:49:40 -0400

    dynamic add button (duvidas)

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 new Vue({
     el: '#app',
     data: {
-        title: "hello world",
-        link: 'http://google.com',
-        finishedLink: "<a href='https://www.google.com/'>Google</a>"
+        sum: 0,
+        x: 0,
+        y: 0,
+        count: 0,
+        print: ''
     },
     methods: {
-        sayHello: function () {
-            this.title = 'hello!'
-            return this.title;
+        increase: function(count){
+            this.sum += Number(count);
+        },
+        printCounter: function(){
+            console.log('adding: ' + this.count + ' total: ' + this.sum)
+            return print;
         }
+        // updateCoordinates: function(event){
+        //     this.x = event.clientX;
+        //     this.y = event.clientY;
+        // }
     }
 });
