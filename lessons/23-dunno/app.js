7c952e3824a81de519d69fe6fa92b36d464aaec9
Igor Dos Santos Salgado <isantos@br.ibm.com>
2019-10-19 14:07:16 -0400

    24

--- lessons/23-dunno/app.js
+++ lessons/23-dunno/app.js
@@ -1,6 +1,20 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Igor'
-    }
+        name: 'Igor',
+        counter: 0,
+        secondCounter: 0
+    },
+    computed: {
+        output: function(){
+            console.log('computed');
+            return this.counter % 3 == 0 ? 'Primo!' : 'Nao é primo!';
+        }
+    },
+    methods: {
+        result(){
+            console.log('method');
+            return this.counter % 3 == 0 ? 'Primo!' : 'Nao é primo!';
+        }
+    },
 });
