de0de8528735cb69ac9c206249fab7b866c9f611
Igor Salgado <igorssalgado@icloud.com>
2019-10-21 14:53:13 -0300

    30 CSS

--- lessons/27-31/app.js
+++ lessons/27-31/app.js
@@ -1,14 +1,14 @@
 new Vue({
     el: '#app',
-    data:{
-        attachRed: false,
-        color: 'green'
+    data: {
+        color: 'gray',
+        width: 100
     },
     computed: {
-        divClasses: function(){
-            return{
-                red: this.attachRed,
-                blue: !this.attachRed
+        myStyle: function () {
+            return {
+                backgroundColor: this.color,
+                width: this.width + 'px'
             }
         }
     }
