e02f3cafb7b45ab5914fd5997c4b4310005abac0
Igor Dos Santos Salgado <isantos@br.ibm.com>
2019-10-23 09:41:18 -0400

    39

--- lessons/Section 3/34-46/app.js
+++ lessons/Section 3/34-46/app.js
@@ -1,6 +1,10 @@
 new Vue({
     el: '#app',
     data: {
-        show: true
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Igor', age: '27', color: 'red'},
+            {name: 'Tara', age: 'unknown', color: 'blue'}
+        ]
     }
 });
