b98c51de8243fb95c60436d6d783ff1a270d7cda
Igor Dos Santos Salgado <isantos@br.ibm.com>
2019-11-06 13:38:36 -0500

    added disable function on heal and special attack

--- lessons/Section 4 MONSTER_SLAYER/MONSTER_SLAYER/app.js
+++ lessons/Section 4 MONSTER_SLAYER/MONSTER_SLAYER/app.js
@@ -8,12 +8,15 @@ new Vue({
         yourHealth: 0,
         monsterHealth: 0,
         yourStrength: 50,
-        monsterStrength: 2,
+        monsterStrength: 3,
         yourColor: '',
+        healthButtonColor: 'red',
+        specialAttackColor: 'red',
+        healed: false,
         monsterColor: '',
         showHoldButton: false,
         turns: [],
-        healButtonDisabled: true,
+        healButtonDisabled: false,
         specialAttackButtonDisabled: true,
         timesAttack: 0,
         disabledTimes: 10
@@ -25,7 +28,7 @@ new Vue({
             vm.turns = []
             vm.loser = false;
             vm.restart = false;
-            vm.gameIsRunning = true; // exibe a vida, a barra de vida e os botoes para o jogo.
+            vm.gameIsRunning = true;
             vm.monsterHealth = 100;
             vm.yourHealth = 100;
             vm.yourColor = '';
@@ -35,10 +38,13 @@ new Vue({
             if (vm.you == '') {
                 vm.you = 'Player Unknow'
             }
+            vm.healButtonDisabled = true;
+            vm.specialAttackColor = 'red'
+            vm.healthButtonColor = 'red'
+            vm.healed = false
         },
         attack: function () {
             let vm = this;
-            // monster health
             let yourAttack = vm.calculateDamage(5, 10);
             vm.monsterHealth = vm.monsterHealth - yourAttack;
 
@@ -49,30 +55,29 @@ new Vue({
             // console.log('your attack >>> ' + yourAttack)
             vm.monsterAttack(yourAttack + 5)
             vm.timesAttack += 1;
-            if(vm.timesAttack > vm.disabledTimes){
+            if (vm.timesAttack > vm.disabledTimes) {
                 vm.specialAttackButtonDisabled = false;
+                vm.specialAttackColor = 'blue';
                 vm.timesAttack = 0;
-                vm.disabledTimes +=3 
+                vm.disabledTimes += 3
             }
         },
         specialAttack: function () {
             let vm = this;
-            // monster health
             let yourAttack = vm.calculateDamage(10, 20);
             vm.monsterHealth = vm.monsterHealth - yourAttack;
-
-            // console.log('your attack >>> ' + yourAttack)
-            // vm.monsterAttack(yourAttack)
             this.turns.unshift({
                 isPlayer: true,
                 text: vm.you + ' hits monster HARD for ' + yourAttack
             });
 
             vm.specialAttackButtonDisabled = true;
+            vm.specialAttackColor = 'red';
         },
         heal: function () {
             let vm = this;
             let heal = 8;
+            vm.healButtonDisabled = true;
             if (vm.yourHealth == 100) {
                 heal = 0;
                 this.turns.unshift({
@@ -81,7 +86,8 @@ new Vue({
                 });
             }
 
-            if (vm.yourHealth < 100) {
+            if (vm.yourHealth != 100) {
+                vm.healthButtonColor = 'red';
                 if (vm.yourHealth >= 90) {
                     let previousHealth = vm.yourHealth
                     vm.yourHealth = 100
@@ -96,9 +102,8 @@ new Vue({
                     isPlayer: true,
                     text: vm.you + ' healed for ' + Math.abs(heal)
                 });
+
             }
-
-
         },
         giveUp: function () {
             let vm = this;
@@ -107,8 +112,6 @@ new Vue({
         },
         monsterAttack: function (attack) {
             let vm = this;
-            //  console.log('Monster attack')
-            //your health
             let monsterAttack = !attack ? vm.calculateDamage(1, 10) : vm.calculateDamage(1, attack);
             vm.yourHealth = Math.floor(vm.yourHealth - monsterAttack);
 
@@ -116,8 +119,6 @@ new Vue({
                 isPlayer: false,
                 text: 'Monster hits ' + vm.you + ' for ' + monsterAttack
             });
-
-            //  console.log('monster attack >>> ' + monsterAttack)
         },
         calculateDamage: function (min, max) {
             let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -131,20 +132,18 @@ new Vue({
             if (vm.yourHealth <= 0) {
                 vm.yourHealth = 'YOU ARE DEAD';
                 vm.yourColor = 'red'
-
                 vm.gameIsRunning = !vm.gameIsRunning;
-                // quando morre 'yourHealth = 0' 
-                // esconde a vida, a barra de vida e os botoes para o jogo e volta o botao pra começar.
                 vm.restart = true;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'YOU DIED' 
+                });
             }
-
-            if (vm.yourHealth < 100) {
+            if (vm.yourHealth < 100 && vm.healthButtonColor == 'red' && vm.healed == false) {
+                vm.healthButtonColor = 'blue'
+                vm.healed = true;
                 vm.healButtonDisabled = false;
-            } else if (vm.yourHealth == 100) {
-                vm.healButtonDisabled = true;
             }
-
-
         },
         monsterHealth: function (value) {
             let vm = this;
@@ -152,9 +151,11 @@ new Vue({
                 vm.monsterHealth = 'DEAD';
                 vm.monsterColor = 'red'
                 vm.gameIsRunning = !vm.gameIsRunning;
-                // quando morre 'monsterHealth = 0' 
-                // esconde a vida, a barra de vida e os botoes para o jogo e volta o botao pra começar.
                 vm.restart = true;
+                this.turns.unshift({
+                    isPlayer: false,
+                    text: 'MONSTER DIED' 
+                });
             }
         }
     }
