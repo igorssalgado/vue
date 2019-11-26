38940c3e024205729ce6274d62d854b42d0b2ce1
Igor Salgado <igorssalgado@gmail.com>
2019-11-26 15:42:05 -0300

    Section 10 Second Course Project - Wonderful Quotes

---
+++ lessons/Section 10 Wonderful Quotes/Start/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Quotes Added</h3>
+      {{!quoteCount ? quoteCount = 0 : quoteCount }} / {{maxQuotes}}
+      <div class="progress">
+        <div class="progress">
+          <div
+            class="progress-bar"
+            role="progressbar"
+            aria-valuenow="60"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            :style="{width: (quoteCount/maxQuotes) * 100 + '%'}"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quoteCount", "maxQuotes"],
+  meothods: {
+    noQuotes(){
+      return 'No quotes!'
+    },
+    progress(){
+      return quoteCount + ' / ' + maxQuotes
+    }
+  }
+};
+</script>
---
+++ lessons/Section 10 Wonderful Quotes/Start/src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <label>Quote</label>
+        <textarea class="form-control" row="3" v-model="quote" @keydown.enter="createNew"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNew(enter) {
+      console.log(this.quote)
+      if (this.quote !== "") {
+        this.$emit("quoteAdded", this.quote);
+        this.quote = "...";
+      }
+    }
+  }
+};
+</script>
+// QUANDO APERTA ENTER DUAS VEZES O ESPACO EM BRANCO NAO TA EM BRANCO
+<style scoped>
+</style>
