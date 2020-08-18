21acff0a314a87c85aeb60602cfe0b798fb48cb2
Igor Dos Santos Salgado <isantos@br.ibm.com>
2020-08-18 10:28:42 -0400

    exercise 4 done

--- exercises/4/Exercise 4/app.js
+++ exercises/4/Exercise 4/app.js
@@ -3,14 +3,28 @@ new Vue({
   data: {
     effects: 'highlight',
     classe: 'red',
-    classe2: 'big',
-    trueorfalse: 'f'
+    classe2: '',
+    boolean: true,
+    estilo: 'color: purple; font-size: 20px;',
+    width: 0,
+    progressBarColor: 'green'
   },
   methods: {
     startEffect: function () {
       setInterval(() => {
         this.effects != 'shrink' ? this.effects = 'shrink' : this.effects = 'highlight';
       }, 2000);
+    },
+    progressBar: function () {
+      let vm = this;
+      interval = setInterval(function () {
+        vm.width = vm.width + 10;
+        if (vm.width == 400) {
+          clearInterval(interval);
+          return vm.progressBarColor = 'blue';
+        }
+      }, 100);
+
     }
   }
 });
